d3172679e0e91f176f28688b2f8dd5e5d87755eb
Bruno MOREL <bmorel@bmorel-pro.local>
2016-06-23 17:06:18 -0400

    [ADD] PHPOpenCloud create_server with public WAN

--- phpopencloud/firstapp-getting-started_1.php
+++ phpopencloud/firstapp-getting-started_1.php
@@ -5,35 +5,31 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use Symfony\Component\Yaml\Yaml;
 
-try {
-  $configs = Yaml::parse(file_get_contents('clouds.yaml'));
-} catch (ParseException $e) {
-    printf("Unable to parse the YAML string: %s", $e->getMessage());
-}
+try { $configs = Yaml::parse(file_get_contents('clouds.yaml')); }
+catch (ParseException $e) { printf("Unable to parse the YAML string: %s", $e->getMessage()); }
 
-$paramConnection= [
+$openstack = new OpenStack\OpenStack([
     'authUrl' => $configs[ 'clouds' ][ 'internap' ][ 'authUrlV3'],
     'region'  => $configs[ 'clouds' ][ 'internap' ][ 'region_name' ],
     'user'    => [
-        'id'       => $configs[ 'clouds' ][ 'internap' ][ 'auth' ][ 'username' ],
-        'password' => $configs[ 'clouds' ][ 'internap' ][ 'auth' ][ 'password' ],
-        'domain'   => ['id' => $configs[ 'clouds' ][ 'internap' ][ 'auth' ][ 'domain_id' ]]
-    ],
-    'scope'   => ['project' => ['id' => $configs[ 'clouds' ][ 'internap' ][ 'auth' ][ 'project_name' ]]]
-];
-
-$client = new OpenStack\OpenStack($paramConnection);
-print_r( $client );
-$identity = $client->identityV3();
-
-
-$token = $identity->generateToken([
+        'name'      => $configs[ 'clouds' ][ 'internap' ][ 'auth' ][ 'username' ],
+        'password'  => $configs[ 'clouds' ][ 'internap' ][ 'auth' ][ 'password' ],
+        'domain'    => [
+            'name'    => $configs[ 'clouds' ][ 'internap' ][ 'auth' ][ 'domain_name' ]
+        ]
+    ]
+]);
+print_r( $openstack );
+$keystone = $openstack->identityV3();
+$token = $keystone->generateToken([
     'user' => [
         'name'     => $configs[ 'clouds' ][ 'internap' ][ 'auth' ][ 'username' ],
         'password' => $configs[ 'clouds' ][ 'internap' ][ 'auth' ][ 'password' ],
         'domain'   => [
-            'id' => $configs[ 'clouds' ][ 'internap' ][ 'auth' ][ 'domain_id' ]
+            'name' => $configs[ 'clouds' ][ 'internap' ][ 'auth' ][ 'domain_name' ]
         ]
     ]
 ]);
 print_r( $token );
+
+echo( "Done! Congrats\n" );
